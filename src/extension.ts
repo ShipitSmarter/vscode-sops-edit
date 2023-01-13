5fed27a6ac80615f9e949b9f476b0193acfbd6e0
bvanderhorn <bvanderhorn@gmail.com>
2023-01-13 15:18:36 +0100

    feat: decrypt and open tmp file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as sd from 'sops-decoder';
 import * as f from './utilities/functions';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -8,27 +7,23 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-sops-edit.decrypt', (uri, files) => {
 		let fpn = f.dissectPath(files);
 		let tempFileName = `${fpn.filePureName}.tmp.${fpn.extension}`;
-		let tempFilePath = `${fpn.parentPath}/${tempFileName}`;
+		// let tempFilePath = `${fpn.parentPath}/${tempFileName}`;
 
-		// copy and decrypt
-		fs.copyFile(fpn.filePath, tempFilePath, _ => {
-			let terminal: vscode.Terminal = f.cdToLocation(fpn.parentPath);
-			f.executeInTerminal([`sops -i -d ${tempFileName}`,'exit'], terminal);
-	
-			vscode.window.onDidCloseTerminal(t => { 
-				if (t === terminal && t.exitStatus) { 
-					vscode.window.showInformationMessage(`Decrypted file ${fpn.fileName} to ${tempFileName}`);
-	
-					// open
-					let openPath = vscode.Uri.file(fpn.filePath.replace(fpn.fileName, tempFileName));
-					vscode.workspace.openTextDocument(openPath).then( doc => vscode.window.showTextDocument(doc));
-				} 
-			}); 
-		});
+		// 
+		let terminal: vscode.Terminal = f.cdToLocation(fpn.parentPath);
+		f.executeInTerminal([`sops -d ${fpn.fileName} > ${tempFileName}`,'exit'], terminal);
 
-		
+		vscode.window.onDidCloseTerminal(t => { 
+			if (t === terminal && t.exitStatus) { 
+				vscode.window.showInformationMessage(`Decrypted file ${fpn.fileName} to ${tempFileName}`);
 
+				// open
+				let openPath = vscode.Uri.file(fpn.filePath.replace(fpn.fileName, tempFileName));
+				vscode.workspace.openTextDocument(openPath).then( doc => vscode.window.showTextDocument(doc));
+			} 
+		}); 
 	});
+
 	context.subscriptions.push(disposable);
 }
 
