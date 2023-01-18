77a7188a7c8d34c6c6df82cdf140c17be27b23d2
bvanderhorn <bvanderhorn@gmail.com>
2023-01-18 17:45:02 +0100

    fix: lint async items

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.workspace.onDidOpenTextDocument(async (textDocument:vscode.TextDocument) => await filePool.openTextDocumentListener(textDocument)),
-		vscode.workspace.onDidCloseTextDocument(async (textDocument:vscode.TextDocument) => await filePool.closeTextDocumentListener(textDocument)),
-		vscode.workspace.onDidSaveTextDocument(async (textDocument:vscode.TextDocument) => await filePool.saveTextDocumentListener(textDocument)),
+		vscode.workspace.onDidCloseTextDocument( (textDocument:vscode.TextDocument) => filePool.closeTextDocumentListener(textDocument)),
+		vscode.workspace.onDidSaveTextDocument( (textDocument:vscode.TextDocument) => filePool.saveTextDocumentListener(textDocument)),
 		vscode.commands.registerCommand('sops-edit.direct-edit', (_, files:vscode.Uri[]) => filePool.editDirectly(files))
 	);
 }
