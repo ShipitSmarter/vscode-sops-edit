70997fbd45aade5f6c0ba6e6fd5ea57330720cc4
bvanderhorn <bvanderhorn@gmail.com>
2023-05-15 13:13:45 +0200

    fix: fix button logic for multi document files

--- src/utilities/EditorContext.ts
+++ src/utilities/EditorContext.ts
@@ -1,23 +1,38 @@
 import { TextEditor, commands } from "vscode";
-import { isOpenedInPlainTextEditor, isSopsEncryptable, isSopsEncrypted, isEncryptableAndEncrypted} from "./functions";
+import { isOpenedInPlainTextEditor, isSopsEncryptable, isEncryptableAndEncrypted} from "./functions";
 
 export class EditorContext {
     public static set(editor:TextEditor|undefined) : void {
-        void this._setIsEncryptable(editor);
-        void this._setIsSopsEncrypted(editor);
+        void this._setAsync(editor);
     }
 
-    private static async _setIsEncryptable(editor:TextEditor|undefined) : Promise<void> {
-        const isSopsEncryptableBool = editor 
-            ? await isSopsEncryptable(editor.document.uri)  && await isOpenedInPlainTextEditor(editor.document.uri) && !(await isEncryptableAndEncrypted(editor.document.uri))
-            : false;
-        void commands.executeCommand('setContext', 'sops-edit.isEncryptable', isSopsEncryptableBool ); 
+    private static async _setAsync(editor:TextEditor|undefined) : Promise<void> {
+        const isOpenedInPlainTextEditorBool = editor ? await isOpenedInPlainTextEditor(editor.document.uri) : false;
+        if (!isOpenedInPlainTextEditorBool) {
+            void this._setEncryptable(false);
+            void this._setEncrypted(false);
+            return;
+        }
+
+        const isSopsEncryptableBool = editor ? await isSopsEncryptable(editor.document.uri) : false;
+        const isEncryptableAndEncryptedBool = editor ? await isEncryptableAndEncrypted(editor.document.uri) : false;
+        if (isSopsEncryptableBool) {
+            if (isEncryptableAndEncryptedBool) {
+                void this._setEncryptable(false);
+                void this._setEncrypted(true);
+                return;
+            } else {
+                void this._setEncryptable(true);
+                void this._setEncrypted(false);
+                return;
+            }
+        }
     }
 
-    private static async _setIsSopsEncrypted(editor:TextEditor|undefined) : Promise<void> {
-        const isSopsEncryptedBool = editor 
-            ? await isOpenedInPlainTextEditor(editor.document.uri) && await isSopsEncrypted(editor.document.uri)
-            : false;
-        void commands.executeCommand('setContext', 'sops-edit.isEncrypted', isSopsEncryptedBool );
+    private static _setEncryptable(value:boolean) : void {
+        void commands.executeCommand('setContext', 'sops-edit.isEncryptable', value ); 
+    }
+    private static _setEncrypted(value:boolean) : void {
+        void commands.executeCommand('setContext', 'sops-edit.isEncrypted', value );
     }
 }
